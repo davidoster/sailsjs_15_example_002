edce783f779dc19d033b9e50c0d9551d305c82ee
David Oster aka George Pasparakis <paspa@hotmail.com>
2022-02-20 13:50:22 +0200

    Initial with a Many To Many using through

---
+++ Sails002/api/controllers/products-controller.js
@@ -0,0 +1,25 @@
+module.exports = {
+  friendlyName: 'Products controller',
+  description: '',
+  inputs: {
+    // name: { type: 'string', required: true },
+    // price: { type: 'number', required: true },
+    // description: { type: 'string', required: false }
+  },
+  exits: {
+
+  },
+  fn: async function (req, res, inputs) {
+    // console.log(req, res, inputs);
+    // var createdProduct = await Product.create( 
+    //   { 
+    //     name: req.name, 
+    //     price: req.price, 
+    //     description: req.description 
+    //   }).fetch();
+    // All done.
+    let products = Product.find().populate('orders');
+    return products;
+
+  }
+};
